d445f80479349695bca42de53a7ffea7b686c1c0
jacksonlr2 <jacksonlr2@vcu.edu>
2021-05-04 22:01:33 -0400

    Home Page

---
+++ connection.php
@@ -0,0 +1,17 @@
+<?php
+/*error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+$servername = "localhost";
+$username = "jacksonlr2";
+$password = "V00864841";
+$database = "project_jacksonlr2";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+*/
+?>
---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Master Meals</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+<section class="hero">
+    <div class="hero-inner">
+        <h1>Master Meals</h1>
+        <h2>Access hundreds of recipes from across the world at a click of a button</h2>
+        <div class="actions">
+            <a href="https://example.com/" class="btn1">Register</a>
+            <a href="https://example.com/" class="btn2">Sign in</a>
+        </div>
+    </div>
+</section>
+</body>
+</html>
