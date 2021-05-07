53306c3ece0281a98c2268e8ebc668a4e8c4a360
jacksonlr2 <jacksonlr2@vcu.edu>
2021-05-07 09:46:05 -0400

    Update registration form

--- register.php
+++ register.php
@@ -4,42 +4,38 @@
     <title> Registration </title>
     <link rel="stylesheet" type="text/css" href="css/bootstrap-reboot.min.css">
 </head>
+
 <body>
-
-<div>
-
-</div>
-
 <div>
     <form action="register.php" method="post">
         <div class="container">
             <div class="row">
                 <div class="col-sm-3">
                     <h1>Registration</h1>
-                    <p>Fill up the form with correct values</p>
+                    <p>Fill up the form with correct values.</p>
                     <hr class="mb-3">
-                    <label for="firstname"><b>First Name </b></b></label>
-                    <input class="form-control" id="firstname" type="text" name="firstname" required>
+                    <label for="first_name"><b>First Name</b></label>
+                    <input class="form-control" id="first_name" type="text" name="first_name" required>
 
-                    <label for="lastname"><b>Last Name </b></b></label>
-                    <input class="form-control" id="lastname" type="text" name="lastname" required>
+                    <label for="last_name"><b>Last Name</b></label>
+                    <input class="form-control" id="last_name" type="text" name="last_name" required>
 
-                    <label for="email"><b>Email Address </b></b></label>
+                    <label for="email"><b>Email Address</b></label>
                     <input class="form-control" id="email" type="text" name="email" required>
 
-                    <label for="username"><b>Username </b></b></label>
+                    <label for="username"><b>Username</b></label>
                     <input class="form-control" id="username" type="text" name="username" required>
 
-                    <label for="password"><b>Password </b></b></label>
+                    <label for="password"><b>Password</b></label>
                     <input class="form-control" id="password" type="text" name="password" required>
                     <hr class="mb-3">
                     <input class="btn btn-primary" type="submit" id="register" name="create" value="Sign Up">
                 </div>
             </div>
-
         </div>
     </form>
 </div>
+
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 <script src="//cdn.jsdelivr.net/npm/sweetalert2@10"></script>
 <script type="text/javascript">
@@ -57,6 +53,7 @@
                 var password = $('password').val();
 
                 e.preventDefault();
+
                 $.ajax({
                     type:'POST',
                     url: 'process.php',
