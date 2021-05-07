f48693525384083955ab8c388aa4aef7eedb3176
jacksonlr2 <jacksonlr2@vcu.edu>
2021-05-07 17:54:50 -0400

    Fixed typo registration

--- registerProcess.php
+++ registerProcess.php
@@ -8,13 +8,13 @@ if(isset($_POST)){
     $origPassword   = $_POST['password'];
     $password = password_hash($origPassword, PASSWORD_DEFAULT);
 
-    $sql = "INSERT INTO users (first_name, last_name, email, username, password ) VALUES(?,?,?,?,?)";
+    $sql = "INSERT INTO user(first_name, last_name, email, username, password ) VALUES(?,?,?,?,?)";
     $stmtinsert = $conn->prepare($sql);
     $result = $stmtinsert->execute([$first_name, $last_name, $email, $username, $password]);
     if($result){
         echo 'Successfully saved.';
     }else{
-        echo 'There were erros while saving the data.';
+        echo 'There were errors while saving the data.';
     }
 }
 else{
