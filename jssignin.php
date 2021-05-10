b24033b28d324637ebfffbd532c1cbae313f44ab
jacksonlr2 <jacksonlr2@vcu.edu>
2021-05-10 06:40:23 -0400

    Chef Main Page

--- jssignin.php
+++ jssignin.php
@@ -14,13 +14,13 @@ if($result){
     $user = $stmtselect->fetch(PDO::FETCH_ASSOC);
     $user_id = $user['user_id'];
 
-    $sql2 = "SELECT * FROM users_chef WHERE users_chef_id = $user_id";
+    $sql2 = "SELECT * FROM users_chef WHERE user_chef_id = $user_id";
     $adminselect = $conn->prepare($sql2);
     $result2 = $adminselect->execute();
 
-    $sql3 = "SELECT * FROM users_viewer WHERE users_viewer_id = $user_id";
+    $sql3 = "SELECT * FROM users_viewer WHERE user_viewer_id = $user_id";
     $viewerselect = $conn->prepare($sql3);
-    $result3 = $viewerselect->execute([$username]);
+    $result3 = $viewerselect->execute();
 
     if($viewerselect->rowCount() > 0 AND password_verify($password, $user['password'])){
         $_SESSION['userlogin'] = $user;
