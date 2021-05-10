633c161999c5a8858b068a78d5e8ceaacbed44c9
jacksonlr2 <jacksonlr2@vcu.edu>
2021-05-10 02:37:29 -0400

    Account Page Appearance

--- account.php
+++ account.php
@@ -18,6 +18,7 @@ if(isset($_GET['logout'])){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Account</title>
+    <link rel="stylesheet" href="css/detailStyle.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
 <body>
