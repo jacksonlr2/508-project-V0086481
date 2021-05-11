1463f5346cd953d0a9329fde2fc80382d4793187
jacksonlr2 <jacksonlr2@vcu.edu>
2021-05-10 20:21:14 -0400

    Pictures!

--- register.php
+++ register.php
@@ -14,7 +14,7 @@
             <div class="row justify-content-center">
                 <div class="col-sm-3">
                     <h1>Registration</h1>
-                    <p>Fill up the form with correct values.</p>
+                    <p>Please fill out the information below.</p>
                     <hr class="mb-3">
                     <label for="first_name"><b>First Name</b></label>
                     <input class="form-control" id="first_name" placeholder="Enter first name" type="text" name="first_name" required>
